3c72958c9926cedb53cbc882364524754b62e64a
nishantsinghal <nishanthnd@gmail.com>
2016-03-09 16:35:33 +0530

    now comes jquery handy

--- js/myscript.js
+++ js/myscript.js
@@ -1,32 +1,12 @@
-
-		dialog1 = document.querySelector("#dialog-1");
-		menu = document.querySelector("#menu");
-
-		menu.addEventListener("click", function(){
-			var menuToggle=true;
-			if(menuToggle){
-				showMenu();
-				menuToggle = false;
-			}
-		});
-
-		
-		dialog1 = document.querySelector("#dialog-1");
-		menu = document.querySelector("#menu1");
-
-		menu.addEventListener("click", function(){
-			var menuToggle1=true;
-			if(menuToggle1){
-				hideMenu();
-				menuToggle1 = false;
-			}
-		});
-
-
-		function showMenu() {
-			dialog1.style.zIndex = 0;
-		}
-
-		function hideMenu() {
-			dialog1.style.zIndex = -1;
-		}
+$(document).ready(function(){
+    	$("#menu").click(function(){
+       	 	$("#main-content").hide();
+       	 	$("#header").hide();
+        	$("#dialog-1").show();
+    });
+    	$("#menu1").click(function(){
+       	 	$("#main-content").show();
+       	 	$("#header").show();
+        	$("#dialog-1").hide();
+    });
+});
