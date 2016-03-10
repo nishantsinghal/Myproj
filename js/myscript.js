ab22d7379136908680114c26831f264e89b74f13
nishantsinghal <nishanthnd@gmail.com>
2016-03-10 14:08:29 +0530

    modified with validation check on password also

--- js/myscript.js
+++ js/myscript.js
@@ -10,3 +10,24 @@ $(document).ready(function(){
         	$("#dialog-1").hide();
     });
 });
+$(document).ready(function(){
+    $('.pass').keyup(function() {
+        var inputVal = $(this).val();
+        //var characterReg = /^([a-zA-Z0-9]{0,8})$/;
+        if(!(inputVal.match(/[A-Z]/))){
+          $(this).prop('title','Missing Capital letters');
+        }
+         if(!(inputVal.match(/[a-z]/))){
+          $(this).prop('title','Missing small letters');
+        }
+         if(!(inputVal.match(/[0-9]/))){
+          $(this).prop('title','Missing digits');
+        }
+        if(inputVal.length<8){
+          $(this).prop('title','length must be greater than 8');
+        }
+        if(inputVal.match(/^\s*[a-zA-Z0-9,\s]+\s*$/)){
+          $(this).prop('title','Missing special characters');
+        }
+  });
+});
